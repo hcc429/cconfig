68c57babf00779c79a6e3c5cc5ce1940a266287a
thomascheng <thomascheng@synology.com>
2024-12-16 16:09:08 +0800

    add go and python to lsp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,13 +13,22 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "ninja",
+        "rst",
+      },
+    },
+  },
 }
