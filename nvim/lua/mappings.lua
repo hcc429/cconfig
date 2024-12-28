534f5786c21e6765efe1b45a868e294f16108b74
thomascheng <thomascheng@synology.com>
2024-12-28 21:56:20 +0800

    sync config

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,4 +8,11 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 map("n", "<leader>e", ":NvimTreeToggle<CR>", {desc = "toggle nvim tree"})
 map("x", "<leader>p", [["_dP]], {desc = "replace with copied content"})
+
+map("n", "j", "gj")
+map("n", "k", "gk")
+map("n", "H", "^")
+map("n", "L", "$")
+
+-- map("n", "<leader>nf", )
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
